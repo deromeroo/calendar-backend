435f97efac716b861f1ec5886ea72e85bf8ccad0
Diego Romero - deromeroo <deromero707@gmail.com>
2024-02-08 14:43:53 -0400

    :construction: CRUD feature complete

--- controllers/events.js
+++ controllers/events.js
@@ -39,21 +39,89 @@ const createEvent = async (req, res = response) => {
 }
 
 //Update EVENTS
-const updateEvent = (req, res = response) => {
+const updateEvent = async (req, res = response) => {
 
-    res.json({
-        ok: true,
-        msg: 'updateEvent'
-    })
+    const eventId = req.params.id
+    const uid = req.uid
+
+    try {
+        
+        const event = await Event.findById( eventId )
+
+        if( !event ) {
+            return res.status(404).json({
+                ok: false,
+                msg: "Event doesn't exist"
+            })
+        }
+
+        if ( event.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'Invalid credentials to modify event'
+            })
+        }
+
+        const newEvent = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventUpdated = await Event.findByIdAndUpdate(eventId, newEvent, { new: true })
+
+        res.json({
+            ok: true,
+            event: eventUpdated
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: "Contact administration"
+        })
+    }
 }
 
 //DELETE Events
-const deleteEvent = (req, res = response) => {
+const deleteEvent = async (req, res = response) => {
 
-    res.json({
-        ok: true,
-        msg: 'deleteEvent'
-    })
+
+    const eventId = req.params.id
+    const uid = req.uid
+
+    try {
+        
+        const event = await Event.findById( eventId )
+
+        if( !event ) {
+            return res.status(404).json({
+                ok: false,
+                msg: "Event doesn't exist"
+            })
+        }
+
+        if ( event.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'Invalid credentials to delete event'
+            })
+        }
+
+       await Event.findByIdAndDelete(eventId)
+
+        res.json({
+            ok: true,
+            msg: 'Successful event elimination'
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: "Contact administration"
+        })
+    }
 }
 
 
